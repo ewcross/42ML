238854171c936fb40c115412cd7f08a003e618c7
Elliot Cross <elliotcross27@gmail.com>
2020-05-05 21:34:23 +0100

    save

--- day01/polynomial_train.py
+++ day01/polynomial_train.py
@@ -6,7 +6,7 @@
 #    By: ecross <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/05/05 18:33:34 by ecross            #+#    #+#              #
-#    Updated: 2020/05/05 19:18:06 by ecross           ###   ########.fr        #
+#    Updated: 2020/05/05 21:31:20 by ecross           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,47 +25,49 @@ new = add_polynomial_features(x, 10)
 thetas = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 cost_values = []
 
-#degree = 2
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.002, n_cycle=1500)
-#mlr.fit_(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 2
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.002, n_cycle=1500)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
 
-#degree = 3
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
-#mlr.fit_(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 3
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
 
-#degree = 4
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
-#mlr.plot_convergence(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 4
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.0000001, n_cycle=50)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
 
-#degree = 5
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
-#mlr.plot_convergence(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 5
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00000005, n_cycle=15)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
 
-#degree = 6
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
-#mlr.plot_convergence(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 6
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.0000000001, n_cycle=30)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
 
-#degree = 7
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
-#mlr.plot_convergence(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 7
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=5e-12, n_cycle=15)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
 
-#degree = 8
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
-#mlr.plot_convergence(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 8
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=5e-14, n_cycle=40)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
 
-#degree = 9
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
-#mlr.plot_convergence(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 9
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=1e-15, n_cycle=55)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
 
-#degree = 10
-#mlr = MLR(np.array(thetas[:degree + 1]), alpha=0.00005, n_cycle=4000)
-#mlr.plot_convergence(new[:, :degree], y)
-#cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+degree = 10
+mlr = MLR(np.array(thetas[:degree + 1]), alpha=8e-17, n_cycle=20)
+mlr.fit_(new[:, :degree], y)
+cost_values.append(mlr.mse_(y, mlr.predict_(new[:, :degree])[1]))
+
+print(cost_values)
